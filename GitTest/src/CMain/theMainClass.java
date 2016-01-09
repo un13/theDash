0a21d961faa4fa9aaed9fba3fe09693ff8ed5447
Mrinal Sardar <mrinal.k.sardar@gmail.com>
2016-01-10 01:55:12 +0530

    space and \n removed

--- GitTest/src/CMain/theMainClass.java
+++ GitTest/src/CMain/theMainClass.java
@@ -3,9 +3,8 @@ package CMain;
 public class theMainClass {
 
 	public static void main(String[] args) {
-		System.out.println("mrinal\n");
-		System.out.println("kanti");
-		
+		System.out.println("mrinal");
+		System.out.println("kanti");	
 	}
 
 }
