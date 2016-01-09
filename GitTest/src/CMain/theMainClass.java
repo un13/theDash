075e511e9b2daa83fed074e4cea469849968cbc7
Mrinal Sardar <mrinal.k.sardar@gmail.com>
2016-01-10 01:38:14 +0530

    added sysout in main

--- GitTest/src/CMain/theMainClass.java
+++ GitTest/src/CMain/theMainClass.java
@@ -3,7 +3,8 @@ package CMain;
 public class theMainClass {
 
 	public static void main(String[] args) {
-		System.out.println("mrinal");
+		System.out.println("mrinal\n");
+		System.out.println("kanti");
 		
 	}
 
